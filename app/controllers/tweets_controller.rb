6a2855891bf30068c9be304b208db136be646236
y.yoshiki <labiatejoker@gmail.com>
2017-06-13 14:37:14 +0900

    発展学習1まで終了

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,25 @@
+class TweetsController < ApplicationController
+  before_action :move_to_index,except: :index
+
+
+  def index
+    @tweets = Tweet.all.order("created_at DESC").page(params[:id]).per(5)
+  end
+
+  def new
+  end
+
+  def create
+    Tweet.create(tweet_params)
+  end
+
+  private
+  def tweet_params
+    params.permit(:name, :image, :text)
+  end
+
+  def move_to_index
+    redirect_to action: :index unless user_signed_in?
+  end
+
+end
