3e20145ec7133997025982040319f8fb9cb2b549
y.yoshiki <labiatejoker@gmail.com>
2017-06-14 15:10:36 +0900

    発展学習2が終了

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170613052855) do
+ActiveRecord::Schema.define(version: 20170614042430) do
 
   create_table "tweets", force: :cascade do |t|
-    t.string   "name",       limit: 255
     t.text     "text",       limit: 65535
     t.text     "image",      limit: 65535
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id",    limit: 4
   end
 
   create_table "users", force: :cascade do |t|
